7eefaad917a0eeb171fa42fcf60da1ba6c21a5a8
ironzhang <zhanghui@ablecloud.cn>
2019-07-03 23:40:21 +0800

    update

---
+++ shconf/config.sh
@@ -0,0 +1,8 @@
+# common sh config
+
+source ~/.shconf/go_path.sh
+source ~/.shconf/http_proxy.sh
+
+export PATH=$PATH:~/gopath/bin:~/program/node/bin
+export GOPATH=~/gopath
+#export ETCDCTL_API=2
---
+++ shconf/go_path.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+std_gopath_enable() {
+	export GOPATH=~/gopath
+}
+
+ablecloud_gopath_enable() {
+	export GOPATH=~/ablecloud.cn
+}
+
+hisense_gopath_enable() {
+	export GOPATH=~/ablecloud.cn/src/nb-iot-platform
+}
+
+electric_gopath_enable() {
+	export GOPATH=~/ablecloud.cn/src/electric-net
+}
