007efca1e97574a88ebd77e6dd31bd4102514f4a
hzhang <hzhang@aibee.com>
2019-07-04 12:56:32 +0800

    update

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin
 export PATH=$PATH:~/gopath/bin
 #export PATH=$PATH:~/program/node/bin
 export GOPATH=~/gopath
