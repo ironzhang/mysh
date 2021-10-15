3eac36dbd526c862133765ef237a0e313a8dca68
zhanghui <iron.zhang@foxmail.com>
2021-10-15 15:58:46 +0800

    update

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin
-#export PATH=$PATH:/usr/local/opt/mysql-client/bin
+export PATH=$PATH:/usr/local/opt/mysql-client/bin
 export PATH=~/gopath/bin:$PATH
 export PATH=~/local/bin:$PATH
 export PATH=$PATH:~/local/program/node/bin
