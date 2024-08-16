15428ee8c8c4a283f7ce6e033dde80fad2d47bcf
zhanghui <iron.zhang@foxmail.com>
2024-08-16 12:07:18 +0800

    update

--- zsh/export.sh
+++ zsh/export.sh
@@ -5,6 +5,9 @@ export PATH=$PATH:/usr/local/opt/mysql-client/bin
 export PATH=~/gopath/bin:$PATH
 export PATH=~/local/bin:$PATH
 export PATH=$PATH:~/local/program/node/bin
+export PATH=$PATH:~/.superdns/bin
 
 export GOPATH=~/gopath
+
 #export ETCDCTL_API=2
+export IntegrationTest=true
